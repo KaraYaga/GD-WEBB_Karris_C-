e44ae8ab472a38dfafc770f7c97fbab6b7920da7
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-01-31 15:08:43 +0100

    [Money Function]
    Learning how to type numbers and have called functions and storing addresses

--- GD-WEBB_Karris_C++/GD-WEBB_Karris_C++.cpp
+++ GD-WEBB_Karris_C++/GD-WEBB_Karris_C++.cpp
@@ -1,17 +1,27 @@
 #include <iostream>  //Library for Input, Output stream
 using namespace std; //Without using this you do not have access to the std vocabulary and must type things such as "std::cout"
 
+
+//Function EarnMoney
+void EarnMoney(int* wallet, int wage) //The star points to the Address, not the value, so now wallet is found within bankAccount, and includes the variable
+{
+	wallet += wage;
+	//wallet = wallet + wage;
+
+	wallet -= (int)(wage / 4);
+	//Taxation
+}
+
+//Function say hi
 int main()
 {
-	cout << "Hello there!" << endl;
-
+	cout << "Hello there! What name is your Account under?" << endl;
 	//Displays a text
 	//cout = Console Out
 	// <<OUT
 	//Displaying from the console to the computer
 
 	string name;
-
 	//Converts string to answer
 
 	cin >> name;
@@ -20,17 +30,26 @@ int main()
 	// >>IN
 	//Takes from computer and stores it in Console
 
-	cout << "Hello " << name << ", how are you today?" << endl;
+	cout << "Hello " << name << ", welcome to The ArtFX Bank!\n";
 
 	//endl is EndLine and is the same as \n
 
-	int x = 23;
-
+	int money = 50000;
 	//Establishing a value
 
-	cout << "The value " << x << " is stored in " << &x;
-	cout << "The value " << name << " is stored in " << &name;
+	int* bankAccount = &money;
+	//Stores the address of money in BankAccount
 
+	cout << "Your account name " << name << " is stored in : " << &name << endl;
+	cout << "Your balance " << money << " is stored in : " << &money << endl;
+	cout << "You can find your bankAccount at this address : " << &bankAccount << endl;
 	//The & calls the address of where a value is stored
+
+	EarnMoney(bankAccount, 10000);
+	//Pulls the stored address of bankAccount and adds the function of EarnMoney to the given variable
+	// (Function , Variable)
+
+	cout << "After wages and taxes you have : " << money;
+	//Console out text and updated money variable
 
 }
