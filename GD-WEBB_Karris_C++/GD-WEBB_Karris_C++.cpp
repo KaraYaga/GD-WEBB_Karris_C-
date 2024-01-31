c87e53fedd53e792c87f7150133d058cab3f23fd
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-01-31 15:12:27 +0100

    [Pointers]

--- GD-WEBB_Karris_C++/GD-WEBB_Karris_C++.cpp
+++ GD-WEBB_Karris_C++/GD-WEBB_Karris_C++.cpp
@@ -5,10 +5,10 @@ using namespace std; //Without using this you do not have access to the std voca
 //Function EarnMoney
 void EarnMoney(int* wallet, int wage) //The star points to the Address, not the value, so now wallet is found within bankAccount, and includes the variable
 {
-	wallet += wage;
+	*wallet += wage;
 	//wallet = wallet + wage;
 
-	wallet -= (int)(wage / 4);
+	*wallet -= (int)(wage / 4);
 	//Taxation
 }
 
@@ -53,3 +53,31 @@ int main()
 	//Console out text and updated money variable
 
 }
+
+//NOTES
+
+//ADDRESSES
+// 
+//A pointer is a variable that stores the address of an object in memory, and is used to access that object.
+//You can access the address of any variable by using the& character before the name of the variable.
+//If I defined :
+//int x = 12;
+//I can print :
+//cout << ”The value” << x << ” is stored in” << &x;
+
+
+//DECLARING POINTERS
+// 
+// We use the type of the target variable followed by a * character to declare a pointer.
+//If I don’t know yet the value to put inside the pointer, I use nullptr to make sure a random address is not assigned.
+//int x = 12;
+//int* ptrX = &x;
+
+//POINTERS
+//
+//We use the name of the variable know the address of the pointed variable.
+//We use the name of the variable preceded by a* character to use the value of the pointed variable.
+//int x = 12;
+//int* ptrX = &x;
+//cout << ptrX;	//Displays the address of the variable x
+//cout << *prtX;		//Displays 12, the content of the pointed variable (x)
